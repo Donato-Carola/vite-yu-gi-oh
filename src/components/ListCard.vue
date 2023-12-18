88eaed260705010d1c27b1fdc1094fccc5969e9a
Donato-Carola <don94@live.it>
2023-12-18 18:15:50 +0100

    style card

--- src/components/ListCard.vue
+++ src/components/ListCard.vue
@@ -1,29 +1,34 @@
 <template lang="">
-   <section>
-    <article v-for="character in  listaCard">
-    {{character.name}}
-</article>
-   </section>
+  <section class="container">
+    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 g-2 g-lg-3">
+    <article v-for="character in listaCard" :key="character.id" class="col">
+      <singleCard :character="character"/>
+    </article>
+   </div>
+  </section>
 </template>
 <script>
-import { store } from '../js/store.js';
+import { store } from "../js/store.js";
+import singleCard from "./singleCard.vue";
 export default {
-    data(){
-        return{
-            store,
-        }
+  data() {
+    return {
+      store,
+    };
+  },
+  components:{
+    singleCard
+  },
+
+  props: {
+    listaCard: {
+      type: Array,
+      required: true,
     },
-
- props:{
-    listaCard:{
-        type: Array,
-        required:true,
-    }
- }
-
-    
-}
+  },
+};
 </script>
-<style lang="scss">
-    
+<style lang="scss" scoped>
+ 
+
 </style>
---
+++ src/components/singleCard.vue
@@ -0,0 +1,36 @@
+<template lang="">
+    <!--<img :src="character.card_images.image_url_small" alt="image">
+      {{character.name}}
+      {{character.archetype}}-->
+      <div class="card" >
+        <img :src="character.card_images.image_url_small" class="card-img-top" alt="image" />
+        <div class="card-body">
+          <h5 class="card-title">{{character.name}}</h5>
+          <p class="card-text">
+            {{character.archetype}}
+          </p>
+          
+        </div>
+      </div>
+</template>
+<script>
+export default {
+    props:{
+        character:{
+            type: Object,
+            required:true
+        }
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+
+    },
+}
+</script>
+<style lang="scss">
+    
+</style>

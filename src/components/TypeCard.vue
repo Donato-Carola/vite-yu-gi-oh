ae9771c2b78232c8283bedd89ec2b20e30e50878
Donato-Carola <don94@live.it>
2023-12-19 18:53:01 +0100

    select

--- src/components/TypeCard.vue
+++ src/components/TypeCard.vue
@@ -1,19 +1,10 @@
 <template >
-  <div class="dropdown ps-5 pt-3 pb-3">
-    <button
-      class="btn btn-secondary dropdown-toggle"
-      type="button"
-      data-bs-toggle="dropdown"
-      aria-expanded="false"
-    >
-      Dropdown button
-    </button>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item">Action</a></li>
-      <li><a class="dropdown-item">Another action</a></li>
-      <li><a class="dropdown-item">Something else here</a></li>
-    </ul>
-  </div>
+  <select class="form-select w-25 ms-5 mt-3 mb-3" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
 </template>
 
 
